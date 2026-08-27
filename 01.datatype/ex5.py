9aaa2505155e2d04216dd3f594e3a40147ec046c
znh0o <yeojunho1210@dimigo.hs.kr>
2026-08-27 09:50:03 +0900

    add all 02_datatype

--- 01.datatype/ex5.py
+++ 01.datatype/ex5.py
@@ -46,53 +46,54 @@ print(s.count("o"))                 # 등장 횟수
 print("Python" in s)                # 포함(True)
 print("pororo" in s)                  # 미포함(False)
 
-# # 특정 Prefix로 시작하고 있는지 알려주는 문자열 메소드
-# print()       # True
+# 특정 Prefix로 시작하고 있는지 알려주는 문자열 메소드
+print(s.startswith("Python"))       # True
 
-# # 특정 Suffix로 끝나고 있는지 알려주는 문자열 메소드
-# print()         # False
+# 특정 Suffix로 끝나고 있는지 알려주는 문자열 메소드
+print(s.endswith("pororo"))         # False
 
-# # 이전 문자열을 새로운 문자열로 치환하는 문자열 메소드
-# # 문제) replace는 원본 문자열을 바꿀까요? 새로운 문자열을 만들까요?
-# print()     # 전체 치환
-# print()  # 1개만 치환
+# 이전 문자열을 새로운 문자열로 치환하는 문자열 메소드
+# 문제) replace는 원본 문자열을 바꿀까요? 새로운 문자열을 만들까요?
+print(s.replace("Python","C"))     # 전체 치환
+print(s.replace("Python","c", 1))  # 1개만 치환
 
-# # 판별 문자열 메소드 (isXXX())
-# print("123")              # 숫자이면 True
-# print("abc")              # 알파벳이면 True
-# print("abc123")           # 알파벳 + 숫자이면 True
-# print(" \t \n")           # 공백문자이면 True
-# print("hello")            # 소문자이면 True
-# print("HELLO")            # 대문자이면 True
+# 판별 문자열 메소드 (isXXX())
+print("123".isdigit())              # 숫자이면 True
+print("六".isnumeric())
+print("abc".isalpha())              # 알파벳이면 True
+print("abc123".isalnum())           # 알파벳 + 숫자이면 True
+print(" \t \n".isspace())           # 공백문자이면 True
+print("hello".islower())            # 소문자이면 True
+print("HELLO".isupper())            # 대문자이면 True
 
-# # 구분자를 기준으로 문자열을 분리하는 문자열 메소드
-# a = "apple,banana,kiwi"
-# fruits = None                # ","를 기준으로 분리 (기본값 공백)
-# print(fruits)                       # 리스트가 만들어짐
+# 구분자를 기준으로 문자열을 분리하는 문자열 메소드
+a = "apple,banana,kiwi"
+fruits = a.split(",")                # ","를 기준으로 분리 (기본값 공백)
+print(fruits)                       # 리스트가 만들어짐
 
-# # Iterable(반복가능) 객체안의 문자열을 결합하는 문자열 메소드
-# print()             # ","로 연결하여 결합
+# Iterable(반복가능) 객체안의 문자열을 결합하는 문자열 메소드
+print(",".join(fruits))         # ","로 연결하여 결합
 
 
 # =========================================================
 #  🔥 실습 문제
 # =========================================================
 
-# # 1️⃣ 문자열 양쪽 공백 제거 + 대문자 변환
-# s = "  hello world  "
-# result = s.______().______()
-# print(result)                   # ✅ "HELLO WORLD" 출력
+# 1️⃣ 문자열 양쪽 공백 제거 + 대문자 변환
+s = "  hello world  "
+result = s.strip().upper()
+print(result)                   # ✅ "HELLO WORLD" 출력
 
-# # 2️⃣ 이메일 형식 체크
-# email = "abcd@dimigo.hs.kr"
-# print(___ ___ email)            # ✅ "@dimigo.hs.kr"이 포함되어 있으면 True
+# 2️⃣ 이메일 형식 체크
+email = "abcd@dimigo.hs.kr"
+print("dimigo.hs.kr" in email)            # ✅ "@dimigo.hs.kr"이 포함되어 있으면 True
 
-# # 3️⃣ 문자열에서 특정 단어가 몇 번 등장하나?
-# sentence = "the quick brown fox jumps over the lazy dog"
-# print(sentence._____("the"))    # ✅ 2 출력
+# 3️⃣ 문자열에서 특정 단어가 몇 번 등장하나?
+sentence = "the quick brown fox jumps over the lazy dog"
+print(sentence.count("the"))    # ✅ 2 출력
 
-# # 4️⃣ 공백문자를 기준으로 문자열을 리스트로 나누고, 다시 문자열로 결합하기
-# data = "   kim   lee   park   choi   "
-# names = data._____()
-# print(names)                    # ✅ 리스트 출력
-# print(___.____(names))          # ✅ "kim lee park choi" 출력
+# 4️⃣ 공백문자를 기준으로 문자열을 리스트로 나누고, 다시 문자열로 결합하기
+data = "   kim   lee   park   choi   "
+names = data.split()
+print(names)                    # ✅ 리스트 출력
+print((" ").join(names))    # ✅ "kim lee park choi" 출력
